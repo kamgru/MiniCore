ca115d0ce77e05ab896072758bf9a7f9e14815f0
kamgru <kamil.grudzien@hotmail.com>
2016-10-19 16:53:38 +0200

    Add service host test

---
+++ MiniCore.Container.Tests/MiniCore.Container.Wcf.UnitTests/ServiceHostTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniCore.Container.Wcf.UnitTests.Mocks;
+
+namespace MiniCore.Container.Wcf.UnitTests
+{
+    [TestClass]
+    public class ServiceHostTests
+    {
+        [TestMethod]
+        public void ShouldAddBehaviourToDescription()
+        {
+            var container = new MiniCoreContainer();
+            var host = new MiniCoreServiceHost(container, typeof(MockService), new[] { new Uri("net.tcp://mock") });
+            host.Open();
+            host.Close();
+
+            Assert.IsTrue(host.Description.Behaviors.Any(a => a.GetType() == typeof(MiniCoreInstanceProviderBehaviour)));
+        }
+    }
+}
